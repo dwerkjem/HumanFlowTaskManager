7ae8740c4dab20eab813088a56594339df1e7ae5
dwerkjem <derekrneilson@gmail.com>
2024-12-16 06:46:13 -0700

    Add Slint dependencies and implement HelloWorld component in main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+slint::slint!{
+    export component HelloWorld {
+        Text {
+            text: "hello world";
+            color: green;
+        }
+    }
+}
 fn main() {
-    println!("Hello, world!");
+    HelloWorld::new().unwrap().run().unwrap();
 }
