ed27a8929b9f02f42bde7daa5e61937d9e73e9f4
dwerkjem <derekrneilson@gmail.com>
2024-12-19 13:21:16 -0700

    Add Dockerfile for Desktop app and update configuration files

--- apps/Desktop/forge.config.cjs
+++ apps/Desktop/forge.config.cjs
@@ -2,9 +2,7 @@ const { FusesPlugin } = require('@electron-forge/plugin-fuses');
 const { FuseV1Options, FuseVersion } = require('@electron/fuses');
 
 module.exports = {
-  packagerConfig: {
-    asar: true,
-  },
+  packagerConfig: {},
   rebuildConfig: {},
   makers: [
     {
@@ -25,12 +23,6 @@ module.exports = {
     },
   ],
   plugins: [
-    {
-      name: '@electron-forge/plugin-auto-unpack-natives',
-      config: {},
-    },
-    // Fuses are used to enable/disable various Electron functionality
-    // at package time, before code signing the application
     new FusesPlugin({
       version: FuseVersion.V1,
       [FuseV1Options.RunAsNode]: false,
